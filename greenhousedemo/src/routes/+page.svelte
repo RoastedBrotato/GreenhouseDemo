7550679ac68553909125063a87a79ef17a3c7fff
RoastedBrotato <waleedajaz1996@gmail.com>
2023-11-12 06:00:16 +0300

    feat: Added tailwind and applied style to index

--- greenhousedemo/src/routes/+page.svelte
+++ greenhousedemo/src/routes/+page.svelte
@@ -2,8 +2,19 @@
     export let data;
   </script>
 
+<style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.800);
+    }
+  </style>
+
   <ul>
     {#each data.Warehouse as country}
       <li>{country.name}</li>
     {/each}
   </ul>
+
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  
