c1a2557b16b927fbaf0425170bf202db34c1640f
RoastedBrotato <waleedajaz1996@gmail.com>
2023-11-13 17:25:41 +0300

    feat: Added sidebar for nav

---
+++ greenhousedemo/src/lib/SideNavbar.svelte
@@ -0,0 +1,8 @@
+ <div class="bg-gray-800 text-white p-4">
+    <h1 class="text-2xl font-bold mb-4">Greenhouse Demo</h1>
+    <ul>
+    <li><a href="/">Home</a></li>
+      <li><a href="/warehouse">Warehouse</a></li>
+      <li><a href="#">Link 2</a></li>
+    </ul>
+  </div>
--- greenhousedemo/src/routes/+layout.svelte
+++ greenhousedemo/src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script>
     import "../app.css";
+    import Sidebar from "$lib/SideNavbar.svelte"
   </script>
+  <div class="flex h-screen">
+    <!-- Include the sidebar -->
+    <Sidebar />
+  
   <div class="container mx-auto">
     <slot />
   </div>
+</div>
