28e66eb04f0d7445317094d73e0351571377ebb8
RoastedBrotato <waleedajaz1996@gmail.com>
2023-11-13 17:59:02 +0300

    feat: added styles to Warehouse

--- greenhousedemo/src/lib/Warehouse.svelte
+++ greenhousedemo/src/lib/Warehouse.svelte
@@ -1,26 +1,35 @@
 <script>
     export let warehouse, updateWarehouse, deleteWarehouse;
-  </script>
-  
-  <div class="warehouses">
-    <input
-    class="border"
-      type="text"
-      value={warehouse.name}
-      on:input={(e) => {
-        warehouse.name = e.currentTarget.value;
-      }}
-    />
+</script>
 
-    <input
-    class="border"
-      type="text"
-      value={warehouse.address}
-      on:input={(e) => {
-        warehouse.address = e.currentTarget.value;
-      }}
-    />
+<tr
+    class="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600"
+>
+    <td class="whitespace-nowrap px-6 py-4 font-medium">
+        <input
+            class="border"
+            type="text"
+            value={warehouse.name}
+            on:input={(e) => {
+                warehouse.name = e.currentTarget.value;
+            }}
+        />
+    </td>
+    <td class="whitespace-nowrap px-6 py-4">
+        <input
+            class="border"
+            type="text"
+            value={warehouse.address}
+            on:input={(e) => {
+                warehouse.address = e.currentTarget.value;
+            }}
+        />
+    </td>
+    <td class="whitespace-nowrap px-6 py-4">
+        <button class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" on:click={() => updateWarehouse(warehouse)}>Edit</button>
+    </td>
+    <td class="whitespace-nowrap px-6 py-4">
+        <button class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" on:click={() => deleteWarehouse(warehouse)}>Delete</button>
+    </td>
+</tr>
 
-    <button on:click={() => updateWarehouse(warehouse)}>Edit</button>
-    <button on:click={() => deleteWarehouse(warehouse)}>Delete</button>
-  </div>
--- greenhousedemo/src/routes/warehouse/+page.svelte
+++ greenhousedemo/src/routes/warehouse/+page.svelte
@@ -65,32 +65,88 @@
   };
 </script>
 
+<div class="px-10 m-auto max-w-7xl mt-10">
+  <div>
+    <h1 class="text-2xl mb-2 font-bold">Warehouse</h1>
+  </div>
 
-<div class="px-10 m-auto max-w-7xl">
+  <div class="add-warehouse mb-3">
+    <div class="w-full max-w-xs">
+      <h1 class="text-md font-bold">Add New Warehouse</h1>
+      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div class="mb-4">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+            for="username"
+          >
+            Warehouse Name
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="newName"
+            type="text"
+            placeholder="Name"
+            bind:value={newName}
+          />
+        </div>
+        <div class="mb-6">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+            for="password"
+          >
+            Warehouse Address
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="newAddress"
+            type="text"
+            placeholder="Address"
+            bind:value={newAddress}
+          />
+        </div>
+        <div class="flex items-center justify-between">
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            on:click={() => addNewWarehouse()}
+          >
+            Add Warehouse
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
 
+  <h1 class="text-md my-2 font-bold">Warehouse Table</h1>
 
-<div class="add-todo">
-  <input type="text" class="border" bind:value={newName} />
-  <input type="text" class="border" bind:value={newAddress} />
-
-  <button on:click={() => addNewWarehouse()}>Add Warehouse</button>
+  <div class="flex flex-col">
+    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+        <div class="overflow-hidden">
+          <table class="min-w-full text-left text-sm font-light">
+            <thead class="border-b font-medium dark:border-neutral-500">
+              <tr>
+                <th scope="col" class="px-6 py-4">Name</th>
+                <th scope="col" class="px-6 py-4">Address</th>
+                <th scope="col" class="px-6 py-4">Edit</th>
+                <th scope="col" class="px-6 py-4">Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+            {#each warehouses as warehouse}
+              <Warehouse {warehouse} {updateWarehouse} {deleteWarehouse} />
+            {:else}
+              <p>No Warehouses found</p>
+            {/each}
+            <tbody />
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
-
-{#each warehouses as warehouse}
-  <Warehouse {warehouse} {updateWarehouse} {deleteWarehouse} />
-{:else}
-  <p>No Warehouses found</p>
-{/each}
-</div>
-<svelte:window on:keypress={handleKeyPress} />
-
 
 <style>
-  .add-todo {
-    display: flex;
-    margin-bottom: 0.5em;
-  }
-
   :global(.switch) {
     color: lightskyblue;
     cursor: pointer;
